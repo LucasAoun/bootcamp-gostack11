141a2f647604095d44044905cf0e10640d183619
Lucas Aoun <souolucas2009@gmail.com>
2020-12-15 11:50:01 -0300

    useState, immutability, loaders

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Header from './components/Header'
+import './app.css'
+import photo from './assets/Koala.jpg'
+
+
 function App(){
+
+  const [projects, setProjects] = useState(['Teste de desenvolvimento', 'Front-end'])
+
+  function handleAddProject(){
+    setProjects([...projects, `Novo projeto ${Date.now()}`])
+  }
+  console.log(projects)
   return(
-    <Header/>
+    <Header title="project">
+      <img src={photo} alt=""/>
+      <ul>
+        {projects.map(project=>{
+          return(
+          <li key={project}>{project}</li>
+          )})}
+      </ul>
+      <button type="button" onClick={handleAddProject}>Adicionar</button>
+    </Header>
   )
 }
 export default App
