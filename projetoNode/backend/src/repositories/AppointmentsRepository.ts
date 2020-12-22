1939a7e28c2ccdcef1d648b631e8a2dd5f3218fa
LucasAoun <souolucas2009@gmail.com>
2020-12-21 22:33:47 -0300

    database already, model already, postgres add

---
+++ projetoNode/backend/src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,16 @@
+import { isEqual } from 'date-fns'
+import { EntityRepository, Repository } from 'typeorm'
+
+import Appointment from '../model/Appointment'
+
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment> {
+  public findByDate (date: Date): Appointment | null {
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(date, appointment.date)
+    )
+    return findAppointment || null
+  }
+}
+
+export default AppointmentsRepository
