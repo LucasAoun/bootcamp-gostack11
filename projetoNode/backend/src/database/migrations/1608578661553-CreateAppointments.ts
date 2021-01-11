c55b7735a860a408e20f1fb4991f919b00000d3c
LucasAoun <souolucas2009@gmail.com>
2021-01-11 11:00:20 -0300

    database already, post and get with docker

--- projetoNode/backend/src/database/migrations/1608578661553-CreateAppointments.ts
+++ projetoNode/backend/src/database/migrations/1608578661553-CreateAppointments.ts
@@ -10,7 +10,8 @@ export class CreateAppointments1608578661553 implements MigrationInterface {
             name: 'id',
             type: 'varchar',
             isPrimary: true,
-            generationStrategy: 'uuid'
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()'
           },
           {
             name: 'provider',
---
+++ projetoNode/backend/src/routes/appointments.routes.ts
@@ -0,0 +1,34 @@
+import { Router } from 'express'
+import { parseISO } from 'date-fns'
+import { getCustomRepository } from 'typeorm'
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository'
+import CreateAppointmentService from '../services/CreateAppointmentService'
+
+const appointmentsRouter = Router()
+
+appointmentsRouter.get('/', async (req, res) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository)
+  const appointments = await appointmentsRepository.find()
+  return res.json(appointments)
+})
+
+appointmentsRouter.post('/', async (req, res) => {
+  try {
+    const { provider, date } = req.body
+
+    const parsedDate = parseISO(date)
+
+    const CreateAppointment = new CreateAppointmentService()
+
+    const appointment = await CreateAppointment.execute({
+      date: parsedDate,
+      provider
+    })
+    return res.json(appointment)
+  } catch (err) {
+    return res.status(400).json({ error: err.message })
+  }
+})
+
+export default appointmentsRouter
